9656b9d713627206731f63869b1d327e5cd18f69
T45K <tasktas9@gmail.com>
2025-06-29 14:58:42 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.arrow-kt:arrow-core:2.1.0")
+}
---
+++ src/main/kotlin/io/github/t45k/either_trial/main.kt
@@ -0,0 +1,29 @@
+package io.github.t45k.either_trial
+
+import arrow.core.EitherNel
+import arrow.core.Nel
+import arrow.core.left
+import arrow.core.nonEmptyListOf
+import arrow.core.raise.either
+import arrow.core.raise.zipOrAccumulate
+
+fun main() {
+    println(incorrectBehavior()) // Either.Left(NonEmptyList(1))
+    println(correctBehavior()) // Either.Left(NonEmptyList(NonEmptyList(1), NonEmptyList(1)))
+}
+
+fun incorrectBehavior(): EitherNel<String, Unit> = either {
+    zipOrAccumulate(
+        { validateList().bind() },
+        { validateList().bind() },
+    ) { _, _ -> }
+}
+
+fun correctBehavior(): EitherNel<Nel<String>, Unit> = either {
+    zipOrAccumulate(
+        { validateList().bind() },
+        { validateList().bind() }
+    ) { _, _ -> }
+}
+
+fun validateList(): EitherNel<String, Unit> = nonEmptyListOf("1").left()
